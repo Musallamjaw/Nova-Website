944ab7622e147d50665ccd0f8360f7cd4877f9c7
ameer <abadran281@gmail.com>
2024-12-14 14:54:40 +0200

    first commit

--- src/router/routers.jsx
+++ src/router/routers.jsx
@@ -8,42 +8,42 @@ import Home from "../pages/Home";
 import AboutUs from "../pages/AboutUs";
 import Contact from "../pages/Contact";
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <RootLayout />,
-//     children: [
-//       {
-//         index: true,
-//         element: (<Home />),
-//       },
-//       {
-//         path: '/aboutUs',
-//         element: (<AboutUs />)
-//       },
-//       {
-//         path:"/contactUs",
-//         element:(<Contact/>)
-//       }
-//     ],
-//   },
-// ],
-//   {
-//     basename: "/Twwir/",
-//   }
-// );
-
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
     children: [
-      { index: true, element: <Home /> },
-      { path: "/aboutUs", element: <AboutUs /> },
-      { path: "/contactUs", element: <Contact /> },
+      {
+        index: true,
+        element: (<Home />),
+      },
+      {
+        path: '/aboutUs',
+        element: (<AboutUs />)
+      },
+      {
+        path:"/contactUs",
+        element:(<Contact/>)
+      }
     ],
   },
-]);
+],
+  {
+    basename: "/Twwir2/",
+  }
+);
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <RootLayout />,
+//     children: [
+//       { index: true, element: <Home /> },
+//       { path: "/aboutUs", element: <AboutUs /> },
+//       { path: "/contactUs", element: <Contact /> },
+//     ],
+//   },
+// ]);
 
 export default function AppRouter() {
   return <RouterProvider router={router} />;
